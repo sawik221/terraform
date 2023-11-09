38ad1f753bd307bc10c06bf5d871048c9b60d332
Bartłomiej Sawicki <b.sawicki@c02fp20cmd6r.olympus.f5net.com>
2023-11-09 15:20:26 +0100

    Second commit

--- main.tf
+++ main.tf
@@ -1,10 +1,32 @@
 provider "aws" {
   region = "eu-north-1"
 }
+
 resource "aws_instance" "example" {
-  ami           = "ami-0fe8bec493a81c7da"
-  instance_type = "t3.micro"
+  ami                    = "ami-0fe8bec493a81c7da"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
   tags = {
-    Name = "terraform-1stExample"
-    }
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
